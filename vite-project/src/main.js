33d4412198a243658bb50432994570c3f3c9ccb1
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-10 16:13:03 +0200

    resettato progetto iniziale + aggiunta cartella img

---
+++ vite-project/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+/*
+
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+
+Open-Sans per i testi
+
+Open-Sans-Condensed per i titoli
+
+*/
