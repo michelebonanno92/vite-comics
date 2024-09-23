f4c819402bdda52d145916404bb9226db2f2c890
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-23 10:45:29 +0200

    aggiornamento

--- vite-project/src/components/HeaderTop.vue
+++ vite-project/src/components/HeaderTop.vue
@@ -2,7 +2,10 @@
     export default {
         data(){
             return{
-                mess: 'ciao mondo',        
+            navLink:{
+                nameLink:'CHARACTERS',
+                
+            }
             };
         },
         methods:{
@@ -14,21 +17,53 @@
 
 <template>
 
-    <header class="container">
-        hghghhg
+    <header>
+        <div class="container">
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="logo">
+            </div>
+             <div>
+                <ul>
+                  
+                </ul>
+
+            </div>
+        </div>
+   
     </header>
  
 </template>
 
+<!-- per istallare SASS va installato nel terminale con il comando "npm add -D sass" -->
+<style lang="scss" scoped>
+    header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-<style scoped>
-    .container{
-        background-color: lightgreen;
+        .container{
+        width: 1300px;
+        margin: 0 20px;
+        padding: 0;
+        background-color: #FFFFFF;
         height: 150px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        ul {
+            display: flex;
+            list-style-type: none;
+        } a{
+            text-decoration: none;
+            color: black;
+            margin: 0 10px ;
+            font-size: 0.6rem;
+        }
     }
-    .prova{
-        font-size: 20rem;
-        color: red ;
+
+
     }
+    
 
 </style>
